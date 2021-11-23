bf262f7974c0cc43cbcd9fe39e22f9c17047857e
Mahdy Noureddine <mahdy.a.noureddine@gmail.com>
2021-11-22 20:25:34 -0500

    commit tree a1e673b9ab2d22f1f571a76e8102034f70dcfcb3

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(block) {
+    if (block >= 42) {
+        return block-42;
+    }
+    else{
+        return 42-block;
+    }
+}
+
+function distanceFromHqInFeet(block) {
+    return distanceFromHqInBlocks(block)*264;
+}
+
+function distanceTravelledInFeet(start,destination) {
+    if (destination >= start){
+        return (destination-start)*264;
+    }
+    else {
+        return (start-destination)*264;
+    }
+}
+
+function calculatesFarePrice(start, destination) {
+    let distance=distanceTravelledInFeet(start,destination);
+    let fare;
+    if (distance<400) {
+        fare = 0;
+        return fare;
+    }
+
+    else if (distance >= 400 && distance < 2000) {
+        fare = (distance-400)*0.02;
+        return fare;
+    }
+
+    else if (distance >= 2000 && distance <= 2500) {
+        fare = 25;
+        return fare;
+    }
+
+    else {
+        fare='cannot travel that far';
+        return fare;
+    }
+}
+
